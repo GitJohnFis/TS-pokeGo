e4b5d05ed2d72e1aed98e3c7c8c42e0ec11357d1
GitJohnFis <jfisiiahi69@gmail.com>
2025-05-05 02:10:09 +0000

    update API and entire program states

--- root/dist/main.js
+++ root/dist/main.js
@@ -1,1 +1,7 @@
-"use strict";
+import { startREPL } from "./repl.js";
+import { initState } from "./state.js";
+async function main() {
+    const state = initState();
+    await startREPL(state);
+}
+main();
